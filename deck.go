9dd3552672cb16f521e1d7e04a09b563cb9f4d7e
Daniel Adhi Nugraha <danielanugr@yahoo.co.id>
2024-04-27 15:31:27 +0800

    transform deck slice into string
    Signed-off-by: Daniel Adhi Nugraha <danielanugr@yahoo.co.id>

--- deck.go
+++ deck.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Create new type of 'Deck' which is a slice of string
 type deck []string
@@ -28,4 +31,8 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
 }
