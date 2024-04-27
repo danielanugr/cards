ddc8dabdcd63cd7221953cbe90bdf9e056b21410
Daniel Adhi Nugraha <danielanugr@yahoo.co.id>
2024-04-27 16:43:20 +0800

    adjust newDeck function to match test requirement
    Signed-off-by: Daniel Adhi Nugraha <danielanugr@yahoo.co.id>

--- deck.go
+++ deck.go
@@ -14,7 +14,7 @@ func newDeck() deck {
 	cards := deck{}
 
 	cardSuits := []string{"Spades", "Hearts", "Clubs", "Diamonds"}
-	cardValues := []string{"Ace", "Two", "Three", "Four", "Five"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
