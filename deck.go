61b43fc9700ed6e1e5a2d625153253adf3327f6c
Daniel Adhi Nugraha <danielanugr@yahoo.co.id>
2024-04-27 15:11:44 +0800

    init project

---
+++ deck.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// Create new type of 'Deck' which is a slice of string
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Clubs", "Diamonds"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i+1, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
