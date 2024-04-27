2a0f5f9763f5b23bea5de23e0c1aa84cf996d6a5
Daniel Adhi Nugraha <danielanugr@yahoo.co.id>
2024-04-27 17:40:38 +0800

    add test for IO functionality
    Signed-off-by: Daniel Adhi Nugraha <danielanugr@yahoo.co.id>

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -16,4 +19,17 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "King of Diamonds" {
 		t.Errorf("Expected first card of King of Diamonds but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToFileAndLoadFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	d := newDeck()
+	d.saveToFile("_decktesting")
+	loadedDeck := loadFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
